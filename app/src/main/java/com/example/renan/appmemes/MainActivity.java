a92261db568bb3ff8f21a999085654e6c92d902e
Renan Alves <renan.alves@aluno.uece.br>
2017-10-21 00:49:46 -0300

    audio e lists funcionando

--- app/src/main/java/com/example/renan/appmemes/MainActivity.java
+++ app/src/main/java/com/example/renan/appmemes/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.renan.appmemes;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -11,22 +13,42 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mp ;
 
     ListView mListView;
 
     //frases diversas
-    String[] frasesDiversas = {"Acabou o tempo de rir",
-            "Você entende isso ou não ?",
-            "Mas o que é isso ?",
-            "Puta que pariu !",
-            "Véio viado !",
-            "País de retardados",
-            "Gordo baleiudo",
-            " É você que vai me ensinar ?",
-            " Você tá rindo ?",
-            "Sou eu o doido",
+    int[] audiosRage = new int[]{R.raw.comosefosse,
+            R.raw.ehvocequevaimeensinar,
+            R.raw.esseehumporra,
+            R.raw.eusouumbosta,
+            R.raw.gordobaleiudotetudo,
+            R.raw.maiorroubodahistoria,
+            R.raw.naotemgraca,
+            R.raw.paisderetardados,
+            R.raw.putaquepariu,
+            R.raw.veioviado,
+            R.raw.vocedestroiessepais,
+            R.raw.voceehumidiota,
+            R.raw.voceentendeisso,
+            R.raw.vocetarindo
+
+    };
+
+    String[] frasesDiversas = {"Como se fosse a maior piada do mundo",
+            "É você que vai me ensinar ?",
+            "Esse é um porra",
             "Eu sou um bosta",
-            "Dilma é o caralho",
+            "Gordo baleiudo",
+            "Maior roubo da história",
+            "Não tem graça",
+            "País de retardados",
+            "Puta que pariu",
+            "Véio Viado",
+            "Você destrói esse país",
+            "Você é um idiota",
+            "Você entende isso ?",
+            "Você tá rindo ?"
             };
 
     @Override
@@ -38,6 +60,35 @@ public class MainActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.listView);
         CustomAdapter customAdapter = new CustomAdapter();
         mListView.setAdapter(customAdapter);
+
+        //criando funcionalidades para a lista
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(mp != null && mp.isPlaying())
+                {
+                    mp.stop();
+                   mp.release();
+                    mp = null;
+                }
+
+                mp = MediaPlayer.create(MainActivity.this, audiosRage[position]);
+                mp.start();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStop() {
+        if(mp != null)
+            mp.stop();
+
+        super.onStop();
     }
 
     class CustomAdapter extends BaseAdapter{
@@ -64,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
             ImageView mImageView = (ImageView) view.findViewById(R.id.imageView);
             TextView mTextView = (TextView) view.findViewById(R.id.textView);
+
 
            //  mImageView.setImageResource(R.drawable.ic_play_circle_outline_black_24dp);
             mTextView.setText(frasesDiversas[position]);
